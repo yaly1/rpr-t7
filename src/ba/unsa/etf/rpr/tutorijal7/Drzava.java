a0da223f23571394ab2298bea2698f9f7e06d683
yaly1 <yaly1@etf.unsa.ba>
2018-12-03 19:32:16 +0100

    Attributes of class "Drzava"

--- src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
 public class Drzava {
+    private String naziv;
+    private int brojStanovnika;
+    private double povrsina;
+    private String jedinicaZaPovrsinu;
+    private Grad glavniGrad;
 }
--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -3,6 +3,6 @@ package ba.unsa.etf.rpr.tutorijal7;
 public class Tutorijal {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 }
