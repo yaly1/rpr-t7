6ea9e8ca19866f193d11be18af90e897018b6db6
yaly1 <yaly1@etf.unsa.ba>
2018-12-03 19:46:23 +0100

    Constructor without parameters, setters and getters added in class "Grad"

--- src/ba/unsa/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -3,5 +3,33 @@ package ba.unsa.rpr.tutorijal7;
 class Grad {
     private String naziv;
     private int brojStanovnika;
-    private double [] temperature = new double[1000];
+    private double[] temperature = new double[1000];
+
+    public Grad() {
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
 }
