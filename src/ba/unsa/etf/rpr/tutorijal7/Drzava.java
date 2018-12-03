d0d7ea3b4d41996083d0c7680f7a0bbd01e0b614
yaly1 <yaly1@etf.unsa.ba>
2018-12-03 19:23:12 +0100

    Classes added without body

---
+++ src/ba/unsa/etf/rpr/tutorijal7/Drzava.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.rpr.tutorijal7;
+
+public class Drzava {
+}
--- src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/etf/rpr/tutorijal7/Tutorijal.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal7;
 
-public class Main {
+public class Tutorijal {
 
     public static void main(String[] args) {
 	// write your code here
