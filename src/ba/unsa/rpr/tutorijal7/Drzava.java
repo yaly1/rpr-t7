abec854a59c719fdaeb882313916b497168d4c86
yaly1 <yaly1@etf.unsa.ba>
2018-12-03 19:44:59 +0100

    Constructor without parameters, setters and getters added in class "Drzava"

--- src/ba/unsa/rpr/tutorijal7/Drzava.java
+++ src/ba/unsa/rpr/tutorijal7/Drzava.java
@@ -6,4 +6,49 @@ class Drzava {
     private double povrsina;
     private String jedinicaZaPovrsinu;
     private Grad glavniGrad;
+
+    public Drzava() {
+
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setPovrsina(double povrsina) {
+        this.povrsina = povrsina;
+    }
+
+    public void setJedinicaZaPovrsinu(String jedinicaZaPovrsinu) {
+        this.jedinicaZaPovrsinu = jedinicaZaPovrsinu;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double getPovrsina() {
+        return povrsina;
+    }
+
+    public String getJedinicaZaPovrsinu() {
+        return jedinicaZaPovrsinu;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
 }
