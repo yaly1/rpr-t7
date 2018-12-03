bda9cb0d4912ee2f08e1242e1284a410c065e735
yaly1 <yaly1@etf.unsa.ba>
2018-12-03 19:48:24 +0100

    Constructor without parameters, setter and getter added in class "UN"

--- src/ba/unsa/rpr/tutorijal7/UN.java
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -4,4 +4,16 @@ import java.util.ArrayList;
 
 class UN {
     private ArrayList<Drzava> NizDrzava;
+
+    public UN(ArrayList<Drzava> nizDrzava) {
+        NizDrzava = nizDrzava;
+    }
+
+    public void setNizDrzava(ArrayList<Drzava> nizDrzava) {
+        NizDrzava = nizDrzava;
+    }
+
+    public ArrayList<Drzava> getNizDrzava() {
+        return NizDrzava;
+    }
 }
